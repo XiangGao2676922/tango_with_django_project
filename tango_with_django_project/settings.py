9326e69f31f0c83a41163d98007ee2ba3c6ffc74
XiangGao2676922 <2676922g@student.gla.ac.uk>
2022-02-23 17:58:50 +0000

    chapter9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -31,8 +31,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+LOGIN_URL = 'rango:login'
 # Application definition
+PASSWORD_HASHERS = [ 
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher', 
+    'django.contrib.auth.hashers.BCryptPasswordHasher', 
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher', 
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher', ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -97,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
